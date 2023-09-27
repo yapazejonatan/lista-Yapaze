53f159fc4f839d08a12a29a505c9fd1b30c42848
YAPAZE JONATAN <jonatanyapaze@yahoo.com>
2023-09-27 20:36:11 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlumnosComponent } from './alumnos/alumnos.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlumnosComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
